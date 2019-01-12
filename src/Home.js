1204315ac8e8f18da063fa7bc7bdb303385a0ff5
Adam Kovalcik <akovalci@uwo.ca>
2019-01-12 00:58:53 -0500

    woah

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,8 @@ import './App.css';
   
    
   
-class Home extends Component {       
+class Home extends Component {     
+    test = false  
     constructor(props) {
         super(props);
         this.state = {
@@ -22,18 +23,15 @@ class Home extends Component {
             newProcess:'',
             Processes:[],
             processNum:0,
+            entryAndExit:[],
             graph: {
                 nodes: [
-                    {id: 0, label: 'Node 0',value:10,x:0,y:100,hidden : true},
-                    {id: 9999, label: 'Node 9999',value:10,x:800,y:100,hidden : true},
+                    // {id: 0, label: 0,value:0,x:0,y:100,hidden : false},
+                    // {id: 9999, label: 0,value:0,x:200,y:100,hidden : false},
                     
                 ],
                 edges: [
                     //DO NOT ALTER
-                    {id:0,from:0, to:1,value:0,label:0,arrows:'to'},
-                    {id:1,from:0, to:2,value:0,label:0,arrows:'to'},
-                    {from:5, to:9999,value:10,label:10,arrows:'to'},
-                    {from:6, to:9999,value:10,label:10,arrows:'to'}
                   ]
                },
                options: {
@@ -60,10 +58,11 @@ class Home extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleEdgeChange = this.handleEdgeChange.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleDone = this.handleDone.bind(this);
     }
     
     handleSubmit(event){
-        
+        const num = this.state.processNum;
         const newProcesses = [
             ...this.state.Processes
         ];
@@ -74,20 +73,23 @@ class Home extends Component {
             ...this.state.graph.edges
         ];
         if(this.state.processNum == 0){
-            newNodes.push({id: 1, label: this.state.newL1,value:this.state.newL1,x:200,y:0});
-            newNodes.push({id: 2, label: this.state.newL2,value:this.state.newL2,x:200,y:0});
-            newEdges[2].from = 1;
-            newEdges[3].from = 2;
+            newNodes.push({id: 0, label: 0,value:0,x:0,y:100,hidden : false});
+            
+            newNodes.push({id: 1, label: parseInt(this.state.newL1),value:parseInt(this.state.newL1),x:(200),y:0});
+            newNodes.push({id: 2, label: parseInt(this.state.newL2),value:parseInt(this.state.newL2),x:(200),y:200});
+            newEdges.push(
+                {from:0, to:1,value:0,label:0,arrows:'to'},
+                {from:0, to:2,value:0,label:0,arrows:'to'}
+            );
         }
         else{
-            newNodes.push({id: this.state.processNum*2-1, label: this.state.newL1,value:this.state.newL1,x:200,y:0});
-            newNodes.push({id: this.state.processNum*2, label: this.state.newL2,value:this.state.newL1,x:200,y:0});
-            newEdges.push({from: this.state.processNum*2-3, to: this.state.processNum*2-1,value:0,label:0,arrows:'to'})
-            newEdges.push({from: this.state.processNum*2-2, to: this.state.processNum*2,value:0,label:0,arrows:'to'})
-            newEdges.push({from: this.state.processNum*2-3, to: this.state.processNum*2,value:this.state.transitL1,label:this.state.transitL1,arrows:'to'})
-            newEdges.push({from: this.state.processNum*2-2, to: this.state.processNum*2-1,value:this.state.transitL2,label:this.state.transitL2,arrows:'to'})
-            newEdges[2].from = this.state.processNum*2-1;
-            newEdges[3].from = this.state.processNum*2;
+            console.log(num);
+            newNodes.push({id: (num+1)*2-1, label: parseInt(this.state.newL1),value:parseInt(this.state.newL1),x:((num+1)*200),y:0});
+            newNodes.push({id: (num+1)*2, label: parseInt(this.state.newL2),value:parseInt(this.state.newL2),x:((num+1)*200),y:200});
+            newEdges.push({from: (num)*2-1, to: (num+1)*2-1,value:0,label:0,arrows:'to'})
+            newEdges.push({from: (num)*2, to: (num+1)*2,value:0,label:0,arrows:'to'})
+            newEdges.push({from: (num)*2-1, to: (num+1)*2,value:parseInt(this.state.transitL1),label:parseInt(this.state.transitL1),arrows:'to'})
+            newEdges.push({from: (num)*2, to: (num+1)*2-1,value:parseInt(this.state.transitL2),label:parseInt(this.state.transitL2),arrows:'to'})
         }
         var newProcessNum = this.state.processNum;
         newProcessNum++;
@@ -110,6 +112,46 @@ class Home extends Component {
         
         console.log(this.state);
     }
+    handleDone(event){
+        
+        if(!this.test){
+            const num = this.state.processNum;
+            const newNodes = [
+                ...this.state.graph.nodes
+            ]
+            const newEdges = [
+                ...this.state.graph.edges
+            ];
+            newNodes.push({id: 9999, label: 0,value:0,x:num*200+200,y:100,hidden : false});
+            newEdges.push(
+                {from:(num*2-1), to:9999,value:0,label:0,arrows:'to'},
+                {from:num*2, to:9999,value:0,label:0,arrows:'to'}
+            );
+            newEdges[newEdges.length-1].value = this.state.entryAndExit[3];
+            newEdges[newEdges.length-2].value = this.state.entryAndExit[2];
+            newEdges[1].value = this.state.entryAndExit[1];
+            newEdges[0].value = this.state.entryAndExit[0];
+            
+            this.setState({
+                ...this.state,
+                graph: {
+                    ...this.state.graph,
+                    nodes:[
+                        ...newNodes
+                    ],
+                    edges: [
+                        ...newEdges
+                    ]
+                } 
+            });
+            this.test = true;
+        }
+        else{
+            console.log(this.state)
+        }
+        
+        
+    }
     handleChange(event){
         this.setState({
             ...this.state,
@@ -117,35 +159,26 @@ class Home extends Component {
         });
     }
     handleEdgeChange(e, index) {
-        const newEdges = [
-            ...this.state.graph.edges
+        const newEntryAndExit = [
+            ...this.state.entryAndExit
         ];
-        newEdges[index].label = parseInt(e.target.value);
-        newEdges[index].value = parseInt(e.target.value);
+        newEntryAndExit[index] = parseInt(e.target.value);
         this.setState({
             ...this.state,
-            graph: {
-                ...this.state.graph,
-                edges: [
-                    ...newEdges
-                ]
-            }   
+            entryAndExit : newEntryAndExit
         });
-
-        console.log(this.state);
 
     }
     render() {
         return (
                 
                 <Container>
-                    <p>DANE{this.state.graph.edges[0].label}</p>
                     <Form >
                         <Form.Group widths='equal'>
                             <Form.Input fluid type="number"  label='Line 1 Entry Cost' onChange={e => this.handleEdgeChange(e, 0)}  />
                             <Form.Input fluid type="number"  label='Line 2 Entry Cost' onChange={e => this.handleEdgeChange(e, 1)}  />
-                            <Form.Input fluid type="number" label='Line 1 Exit Cost'  />
-                            <Form.Input fluid type="number" label='Line 2 Exit Cost'  />
+                            <Form.Input fluid type="number" label='Line 1 Exit Cost'  onChange={e => this.handleEdgeChange(e, 2)}/>
+                            <Form.Input fluid type="number" label='Line 2 Exit Cost'  onChange={e => this.handleEdgeChange(e, 3)}/>
                         </Form.Group>
                         {/* <Button type='submit'>Submit</Button> */}
                     </Form>
@@ -162,11 +195,16 @@ class Home extends Component {
                         </Form.Group>
                         <Button type='submit'>Add Process</Button>
                     </Form>
+                    <hr></hr>
+                    <Form onSubmit={this.handleDone}>
+                        <Button type='Submit'>Done</Button>
+                    </Form>
                 <h1>Processes</h1>
                 <div>
                     {this.state.Processes.map((p, i) => <h4 key={i}  >{p}</h4>)}
                 </div>
                 <Graph graph={this.state.graph} options={this.state.options} events={this.state.events} />
+                
                 </Container>           
         );
     }
